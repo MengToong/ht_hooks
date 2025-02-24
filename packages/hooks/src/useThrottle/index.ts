aafc61b4c7113ac0a2bedaef9fec557c68e1c88e
MT <971590939@qq.com>
2025-02-24 21:36:47 +0800

    笔记

--- packages/hooks/src/useThrottle/index.ts
+++ packages/hooks/src/useThrottle/index.ts
@@ -17,3 +17,13 @@ function useThrottle<T>(value: T, options?: ThrottleOptions) {
 }
 
 export default useThrottle;
+
+
+//!课上的
+type Callback=(...args:any[])=>void //定义了一个可以接受任意数量类型参数的，并且没有返回值的回调函数类型
+
+const useThrottle2=(callback:Callback,delay:number=500)=>{
+  const throttleCallback=()=>{
+    
+  }
+}
