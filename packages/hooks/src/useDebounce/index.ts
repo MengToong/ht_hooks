3fafc19d68102080d1976bbfc2cbd99a9908a2ec
MT <971590939@qq.com>
2025-02-24 21:18:09 +0800

    笔记

--- packages/hooks/src/useDebounce/index.ts
+++ packages/hooks/src/useDebounce/index.ts
@@ -17,3 +17,19 @@ function useDebounce<T>(value: T, options?: DebounceOptions) {
 }
 
 export default useDebounce;
+
+
+//!课上
+const useDebounce2=(value:any,delay:number=500)=>{ //#参数value经过useDebounce2过滤输出的debounceValue取一段时间value没变的最新value
+  const [debounceValue,setDebouncedValue]=useState(value);
+
+  useEffect(()=>{
+    const timer=setTimeout(()=>{
+      setDebouncedValue(value)
+    },delay)
+    return ()=>{clearTimeout(timer)}
+  },
+  [value,delay] //#value变化时，执行上一次return（清除定时器）和这次内容（开启新定时器）
+  )
+  return debounceValue//#定时器触发 setState，会导致组件重新渲染，useDebounce2 会再次执行，返回最新的 debouncedValue
+}
