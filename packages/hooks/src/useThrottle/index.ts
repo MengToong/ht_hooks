c008a8804c1f2074893f66ba806c90c055bc0e16
MT <971590939@qq.com>
2025-02-25 10:23:13 +0800

    笔记

--- packages/hooks/src/useThrottle/index.ts
+++ packages/hooks/src/useThrottle/index.ts
@@ -20,10 +20,23 @@ export default useThrottle;
 
 
 //!课上的
-type Callback=(...args:any[])=>void //定义了一个可以接受任意数量类型参数的，并且没有返回值的回调函数类型
+type fn=(...args:any[])=>void //定义了一个可以接受任意数量类型参数的，并且没有返回值的回调函数类型
 
-const useThrottle2=(callback:Callback,delay:number=500)=>{
-  const throttleCallback=()=>{
+// const useThrottle2=(callback:Callback,delay:number=500)=>{
+//   const throttleCallback=()=>{
     
-  }
+//   }
+// }
+
+function useThrottle3(fn:fn, t:number) {
+  let timer:NodeJS.Timeout|null = null;
+
+  return function () {
+    if (!timer) { //#若timer有值说明正在计时中则什么都不做，若timer没值（计时结束定时器自动清除，timer手动清空）说明此时无计时，则开启定时，结束后执行然后关闭定时器
+      timer = setTimeout(() => {
+        fn();
+        timer = null;
+      }, t);
+    }
+  };
 }
