d67db2211f297ef8bc487e83a86018a0d9fd3f89
MT <971590939@qq.com>
2025-02-25 11:36:01 +0800

    笔记

--- packages/hooks/src/useThrottle/index.ts
+++ packages/hooks/src/useThrottle/index.ts
@@ -30,6 +30,11 @@ type fn=(...args:any[])=>void //定义了一个可以接受任意数量类型参
 
 function useThrottle3(fn:fn, t:number) {
   let timer:NodeJS.Timeout|null = null;
+  useEffect(()=>{ //#使useThrottle3所在组件被卸载时若正在计时则清除定时器，防止执行定时器回调导致问题
+    return()=>{
+      if(timer)clearTimeout(timer)
+    }
+  })
 
   return function () {
     if (!timer) { //#若timer有值说明正在计时中则什么都不做，若timer没值（计时结束定时器自动清除，timer手动清空）说明此时无计时，则开启定时，结束后执行然后关闭定时器
