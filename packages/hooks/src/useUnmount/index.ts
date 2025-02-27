3bf539d74e383e58e395b4c59b768dfd0391a434
MT <971590939@qq.com>
2025-02-27 12:43:52 +0800

    笔记

--- packages/hooks/src/useUnmount/index.ts
+++ packages/hooks/src/useUnmount/index.ts
@@ -6,15 +6,15 @@ import isDev from '../utils/isDev';
 const useUnmount = (fn: () => void) => {
   if (isDev) {
     if (!isFunction(fn)) {
-      console.error(`useUnmount expected parameter is a function, got ${typeof fn}`);
+      console.error(`useUnmount expected parameter is a function, got ${typeof fn}`);//这个报错是因为useUnmount的参数必须是一个函数
     }
   }
 
-  const fnRef = useLatest(fn);
+  const fnRef = useLatest(fn);//!使用useLatest保存fn，实时更新最新的fn，且能获取到最新的fn不落入闭包陷阱
 
   useEffect(
     () => () => {
-      fnRef.current();
+      fnRef.current();//!调用useEffect实现卸载时执行
     },
     [],
   );
